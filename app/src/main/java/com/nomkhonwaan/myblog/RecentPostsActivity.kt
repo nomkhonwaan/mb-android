06926e515e0a41441d1ae6770cc64e8fd7a9c3bf
Natcha Luangaroonchai <natcha.luangaroonchai@krungthai-axa.co.th>
2018-05-25 14:31:05 +0700

    Create simple sidebar navigation and sidebar toggle button

--- app/src/main/java/com/nomkhonwaan/myblog/RecentPostsActivity.kt
+++ app/src/main/java/com/nomkhonwaan/myblog/RecentPostsActivity.kt
@@ -1,13 +1,26 @@
 package com.nomkhonwaan.myblog
 
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_recent_posts.*
+import kotlin.math.floor
 
 class RecentPostsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recent_posts)
+
+        btnSidebarToggle.setOnClickListener {
+            val params: ViewGroup.MarginLayoutParams = llApp.layoutParams as ViewGroup.MarginLayoutParams
+            val marginStart: Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (256 * -1).toFloat(), Resources.getSystem().displayMetrics).toInt()
+            params.setMargins(if (params.leftMargin != 0) 0 else marginStart, params.topMargin, params.rightMargin, params.bottomMargin)
+            llApp.requestLayout()
+        }
     }
 
 }
