05ccdb6a7173454d39e687516f760458e6552901
Natcha Luangaroonchai <me@nomkhonwaan.com>
2018-07-03 20:39:00 +0700

    [WIP] Add close button on sidebar header

--- app/src/main/java/com/nomkhonwaan/mb/MainActivity.kt
+++ app/src/main/java/com/nomkhonwaan/mb/MainActivity.kt
@@ -9,7 +9,6 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.DisplayMetrics
 import android.util.TypedValue
 import android.view.View
-import android.view.ViewGroup
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.partial_app_header.*
@@ -24,45 +23,24 @@ class MainActivity : AppCompatActivity() {
         val outMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(outMetrics)
 
+        // Set the main width to equal with a screen width
         main.layoutParams.width = outMetrics.widthPixels
+
+        // Set the app width to equal with a sum of the main width plus sidebar width
         app.layoutParams.width = main.layoutParams.width + sidebar.layoutParams.width
 
-        // Set-up sidebar animation and listen on toggle sidebar events
-        setSidebarAnimation(outMetrics)
-//
-//        setSidebarAnimation(outMetrics)
-//        setSidebarNavItems()
+        // Create an animation set for sidebar and popup_overlay on toggle sidebar
+        createToggleSidebarAnimation(outMetrics)
 
-//        supportFragmentManager
-//                .beginTransaction()
-//                .add(appContent.id, RecentPostsFragment.newInstance())
-//                .commit()
+        // Add list of nav-items to the sidebar navigation (RecyclerView)
+        addSidebarNavItems()
+
+        // Default render a recent-posts page
+        renderRecentPosts()
     }
 
-//    private fun setSidebarNavItems() {
-//        val viewManager = LinearLayoutManager(this)
-//        val viewAdapter = NavItemsViewAdapter(
-//                arrayOf(
-//                        NavItem("Home"),
-//                        NavItem("Login / Register"),
-//                        NavItem("Web Development"),
-//                        NavItem("Web Design"),
-//                        NavItem("Programming"),
-//                        NavItem("DevOps"),
-//                        NavItem("Life Style"),
-//                        NavItem("How I made this website")
-//                )
-//        )
-//
-//        nav.apply {
-//            setHasFixedSize(true)
-//            layoutManager = viewManager
-//            adapter = viewAdapter
-//        }
-//    }
-
-    private fun setSidebarAnimation(outMetrics: DisplayMetrics) {
-        sidebarObservable().subscribe { isCollapsed ->
+    private fun createToggleSidebarAnimation(outMetrics: DisplayMetrics) {
+        toggleSidebarObservable().subscribe { isCollapsed ->
             val animatorSet = AnimatorSet()
 
             if (!isCollapsed) {
@@ -70,11 +48,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             animatorSet.playTogether(
-                    ObjectAnimator.ofFloat(app, "translationX", TypedValue.applyDimension(
-                            TypedValue.COMPLEX_UNIT_DIP,
-                            if (isCollapsed) -256f else 0f,
-                            outMetrics
-                    )),
+                    ObjectAnimator.ofFloat(app, "translationX",
+                            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, if (isCollapsed) -256f else 0f, outMetrics)),
                     ObjectAnimator.ofFloat(popup_overlay, "alpha", if (isCollapsed) 0f else 0.16f)
             )
 
@@ -100,7 +75,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun sidebarObservable(): Observable<Boolean> {
+    private fun toggleSidebarObservable(): Observable<Boolean> {
         return Observable.create { emitter ->
             var isCollapsed = true
             val toggleSidebar = { _: View -> isCollapsed = !isCollapsed; emitter.onNext(isCollapsed) }
@@ -111,4 +86,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun addSidebarNavItems() {
+        val viewManager = LinearLayoutManager(this)
+        val viewAdapter = NavItemsViewAdapter(arrayOf(
+                NavItem(resources.getString(R.string.sidebar_home)),
+                NavItem(resources.getString(R.string.sidebar_login_register)),
+                NavItem(resources.getString(R.string.sidebar_web_development)),
+                NavItem(resources.getString(R.string.sidebar_web_design)),
+                NavItem(resources.getString(R.string.sidebar_programming)),
+                NavItem(resources.getString(R.string.sidebar_devops)),
+                NavItem(resources.getString(R.string.sidebar_life_style)),
+                NavItem(resources.getString(R.string.sidebar_how_i_made_this_website))
+        ))
+
+        nav.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+    }
+
+    private fun renderRecentPosts() {
+        supportFragmentManager
+                .beginTransaction()
+                .add(content.id, RecentPostsFragment.newInstance())
+                .commit()
+    }
 }
